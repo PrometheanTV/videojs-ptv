97e8a79f7b53114be00bdc0e222e7b50082acaa8
Andi Wilson <andi.wilson@promethean.tv>
2020-12-03 09:29:27 -0800

    Freeze constants.

--- src/constants.js
+++ src/constants.js
@@ -1,39 +1,39 @@
 /**
  * API hosts.
  */
-export const ApiHosts = {
+export const ApiHosts = Object.freeze({
   COMDEV: 'comdev.broadcast.promethean.tv',
   QA: 'qa.broadcast.promethean.tv',
   STAGING: 'staging.broadcast.promethean.tv',
   PRODUCTION: 'broadcast.promethean.tv'
-};
+});
 
 /**
  * Embed hosts.
  */
-export const EmbedHosts = {
+export const EmbedHosts = Object.freeze({
   COMDEV: 'comdev.embed.promethean.tv',
   QA: 'qa.embed.promethean.tv',
   STAGING: 'staging.embed.promethean.tv',
   PRODUCTION: 'embed.promethean.tv'
-};
+});
 
 /**
  * Player events.
  */
-export const PlayerEvents = {
+export const PlayerEvents = Object.freeze({
   ENDED: 'ended',
   ERROR: 'error',
   PAUSE: 'pause',
   PLAY: 'play',
   TIME_UPDATE: 'timeupdate'
-};
+});
 
 /**
  * PTV SDK events.
  */
-export const SdkEvents = {
+export const SdkEvents = Object.freeze({
   CLICK_MISS: 'ptv.click.miss',
   CONFIG_FAILURE: 'ptv.config.failure',
   CONFIG_READY: 'ptv.config.ready'
-};
+});
