79ca23715db04bcf28cdf01aacb98f6d770a703e
Albert Bosch <albert@intrepidloop.com>
2020-12-09 17:28:28 +0000

    Test embed is created with the correct params

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -6,6 +6,15 @@ import videojs from 'video.js';
 
 import plugin from '../src/plugin';
 
+const config = {
+  // Test channel and stream
+  channelId: '5c701be7dc3d20080e4092f4',
+  streamId: '5de7e7c2a6adde5211684519',
+  debug: true
+};
+
+const reParam = (key, value) => new RegExp(`\\?.*&${key}=${value}(&|$)`);
+
 const Player = videojs.getComponent('Player');
 
 QUnit.test('the environment is sane', function(assert) {
@@ -46,6 +55,46 @@ QUnit.test('registers itself with video.js', function(assert) {
       this.player.hasClass('vjs-ptv'),
       'the plugin adds a class to the player'
     );
+    done();
+  });
+});
+
+QUnit.test('creates the embed', function(assert) {
+  const done = assert.async();
+
+  assert.expect(6);
+
+  this.player.ptv(config);
+
+  setTimeout(() => {
+    const iframe = this.fixture.querySelector('iframe.ptv-iframe');
+
+    assert.ok(iframe, 'ptv-iframe created');
+
+    const domainRE = new RegExp('^https://embed.promethean.tv/??');
+
+    assert.ok(
+      domainRE.test(iframe.src),
+      'loads from https://embed.promethean.tv'
+    );
+
+    assert.ok(
+      reParam('channelId', config.channelId).test(iframe.src),
+      `uses config.channelId = ${config.channelId}`
+    );
+
+    assert.ok(
+      reParam('streamId', config.streamId).test(iframe.src),
+      `uses config.streamId = ${config.streamId}`
+    );
+
+    assert.ok(
+      reParam('excludePlayer', true).test(iframe.src),
+      'uses excludePlayer = true'
+    );
+
+    assert.ok(reParam('iframe', true).test(iframe.src), 'uses iframe = true');
+
     done();
   });
 });
