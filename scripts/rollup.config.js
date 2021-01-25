7c6af95c584dc82464414dcccd8e8d4f878cbb69
Alexander Syed <alexksyed@gmail.com>
2021-01-25 18:52:40 +0100

    fix typo

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -5,7 +5,7 @@ const generate = require('videojs-generate-rollup-config');
 const options = {};
 const config = generate(options);
 
-// Add additonal builds/customization here!
+// Add additional builds/customization here!
 
 // export the builds to rollup
 export default Object.values(config.builds);
