b0789e4f2b538fc8f256eb0dab5c91c99d993efd
Alexander Syed <alexksyed@gmail.com>
2021-01-27 12:43:04 +0100

    Enhance test to verify asynchronous behaviour of API over `postMessage` and ensure correct behaviour of preload state.

--- test/plugin.api.test.js
+++ test/plugin.api.test.js
@@ -1,9 +1,19 @@
 import document from 'global/document';
+import window from 'global/window';
 import QUnit from 'qunit';
 import videojs from 'video.js';
-import sinon from 'sinon';
 import { ApiHosts } from '../src/constants';
-import {iframeMarkup} from './mocks';
+import { iframeMarkup, PTV_TEST_CALLBACK } from './mocks';
+import PtvEmbed from '../src/embed';
+
+// Supply mock implementations of PtvEmbed static functions.
+// (easier than mocking the module!)
+//
+// Override `assignIframeSource` so that we can load our test iframe content.
+PtvEmbed.assignIframeSource = (el) => el.setAttribute('srcdoc', iframeMarkup);
+// Override `getOrigin` so that we can set the correct origin for the mock iframeMarkup.
+// This should match the origin that the tests are run from
+PtvEmbed.getOrigin = () => 'http://localhost:9999';
 
 const config = {
   // Test channel and stream
@@ -14,9 +24,21 @@ const config = {
   debug: true
 };
 
-QUnit.module('api', function(hooks) {
-  let ptv;
-  let spyPostMessage;
+/**
+ * Performs an asynchronous test for the API on the videojs-ptv host iframe
+ * Each public API method is called before the iframe target has loaded.
+ * The tests then wait for the (mock) iframe target to echo the message that
+ * it received (via its own `window.onmessage`) back to the host window for
+ * test assertions.
+ * Messages are echoed back to the host window via a function assigned to
+ * window[PTV_TEST_CALLBACK] on the host's Window object.
+ *
+ * This allows the following tests to assert the expected behaviour of the
+ * preload state mechanism of the PtvEmbed which tries to ensure that any
+ * API methods called before the SDK has loaded are then correctly applied to
+ * the SDK once it ready.
+ */
+QUnit.module.only('communication with target iframe', function(hooks) {
 
   hooks.beforeEach(function() {
     return new Promise((resolve, _) => {
@@ -24,45 +46,45 @@ QUnit.module('api', function(hooks) {
       this.video = document.createElement('video');
       this.fixture.appendChild(this.video);
       this.player = videojs(this.video);
-      ptv = this.player.ptv(config);
-      const waitForEmbedReady = () => setTimeout(() => {
-        if (ptv.embed && ptv.embed.ready) {
-          // This test may not be good enough due to postMessage origin.
-          spyPostMessage = sinon.spy(ptv.embed.el.contentWindow, 'postMessage');
-          return resolve();
-        }
-        waitForEmbedReady();
-      }, 10);
-
-      waitForEmbedReady();
+      this.ptv = this.player.ptv(config);
+      this.player.ready(()=>resolve());
     });
   });
 
-  hooks.afterEach(function(assert) {
-    ptv.dispose();
-    if (spyPostMessage) {
-      spyPostMessage.restore();
-    }
+  hooks.afterEach(function() {
+    this.ptv.dispose();
+  });
+
+  hooks.after(function() {
+    delete window[PTV_TEST_CALLBACK];
   });
 
   const testFactory = (apiMethod, apiArgs) =>
     function(assert) {
-      ptv[apiMethod](apiArgs);
+      const done = assert.async(1);
 
-      // assert.expect(apiArgs ? 4 : 3);
-      const [{ method, source, value }, origin] = spyPostMessage.lastCall.args;
+      window[PTV_TEST_CALLBACK] = (payload) => {
+        const { method, value } = payload;
 
-      assert.equal(origin, ptv.embed.origin, `origin is ${ptv.embed.origin}`);
-      assert.equal(method, apiMethod, `method is ${apiMethod}`);
-      assert.equal(source, '@ptv-host', 'source is @ptv-host');
-      if (apiArgs) {
-        assert.equal(value, apiArgs, 'args is payload');
-      }
+        assert.equal(apiMethod, method, `iframe received ${method}`);
+        assert.equal(
+          JSON.stringify(value),
+          JSON.stringify(apiArgs),
+          `${method} payload data is correct `
+        );
+        done();
+      };
+      this.ptv[apiMethod](apiArgs);
     };
 
   QUnit.test('ptv.hide() posts correct postMessage', testFactory('hide'));
 
-  QUnit.test('ptv.load() posts correct postMessage', testFactory('load'));
+  QUnit.test('ptv.load() posts correct postMessage', testFactory(
+    'load',
+    {
+      channelId: 'test-channel'
+    }
+  ));
 
   QUnit.test('ptv.show() posts correct postMessage', testFactory('show'));
 
@@ -72,6 +94,6 @@ QUnit.module('api', function(hooks) {
 
   QUnit.test(
     'ptv.timeUpdate() posts correct postMessage',
-    testFactory('timeUpdate', 'payload')
+    testFactory('timeUpdate', 0)
   );
 });
