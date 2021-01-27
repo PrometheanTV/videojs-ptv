8d7f0c0f3c75d720d1b914167e2c26c890df6f17
Alexander Syed <alexksyed@gmail.com>
2021-01-27 12:40:28 +0100

    Enhance iframe test content to echo messages received back to host window via callback function.

--- test/mocks.js
+++ test/mocks.js
@@ -1,7 +1,13 @@
 import { SdkEvents } from '../src/constants';
+export const PTV_TEST_CALLBACK = 'ptv_test_callback';
 
 /**
- * Iframe content for mocking SDK behaviour.
+ * Iframe content that allows us to verify messages sent via `postMessage`.
+ * The mock target listens for messages sent by videojs-ptv and echoes them
+ * back to the host window to a global function at window[PTV_TEST_CALLBACK]()
+ *
+ * Tests can assign a function to window[PTV_TEST_CALLBACK] and assert the
+ * payload that was received by the target via the window 'message' evt.
  * @type {string}
  */
 export const iframeMarkup = `<!DOCTYPE html>
@@ -9,7 +15,14 @@ export const iframeMarkup = `<!DOCTYPE html>
 <head>
 <script>
 console.log('setting up iframe test target');
-window.addEventListener('message', msg => console.log('iframe test target received', msg));
+window.addEventListener('message', evt => {
+  console.log('iframe test target received', evt);
+  // evt.source provides a reference to the host page's Window
+  if (typeof evt.source['${PTV_TEST_CALLBACK}'] === 'function') {
+    console.log('sending to ptv_test_callback');
+    evt.source['${PTV_TEST_CALLBACK}'](evt.data);
+  }
+});
 </script>
 </head>
 <body>
